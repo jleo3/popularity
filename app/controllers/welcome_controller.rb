3c53fb464b6dca61292582910f3f2f08fe80bbd5
jleo3 <joseph.leo3@gmail.com>
2013-10-29 08:20:56 -0400

    config twitter client, paint friends on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+    @tweeps = get_my_tweeps
+  end
+
+  private
+
+  def get_my_tweeps
+    friends = TWITTER_CLIENT.friends.to_a
+    friends.sort_by { |friend| friend.followers_count }.reverse!
   end
 end
