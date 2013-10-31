818e0bae559d37fed01b2602fdb72b885e2391a1
jleo3 <joseph.leo3@gmail.com>
2013-10-31 14:14:16 -0400

    steps 1 and 2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    @tweeps = get_my_tweeps
-  end
-
-  private
-
-  def get_my_tweeps
-    TWITTER_FRIENDS.sort_by { |friend| friend.followers_count }.reverse!
   end
 end
