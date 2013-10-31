6502557a371a421e13cc82bf832d69b299044951
jleo3 <joseph.leo3@gmail.com>
2013-10-31 19:18:14 -0400

    preparing for devise

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,10 +1,6 @@
 TWITTER_CLIENT = Twitter::REST::Client.new do |config|
   config.consumer_key = "F1EbO7iQzBCtumPIWGxRLQ"
   config.consumer_secret = "9kGmSk6rIEffNauBE9rYEpHvzSBzIgd34AbFRvKtuo"
-  config.access_token = "16355050-NjTXfjcrZ32BWsVxekvW11K4ch2ETsGh0acXZS5Na"
-  config.access_token_secret = "fUmojD81aCuahbb2Cm3aWIprJzwHydbp9pkRL5OzwfLBf"
 
   config.connection_options[:request] = {:open_timeout=>10, :timeout=>20}
 end
-
-TWITTER_FRIENDS = TWITTER_CLIENT.friends.to_a
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131029005323) do
-
-  create_table "tweeps", force: true do |t|
-    t.string   "name"
-    t.string   "handle"
-    t.integer  "followers"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
