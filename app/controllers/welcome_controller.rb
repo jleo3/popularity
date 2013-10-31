7dda21fb400b60f82e36a51fbffef553f459d615
jleo3 <joseph.leo3@gmail.com>
2013-10-31 16:34:19 -0400

    back to the way it was

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @user = current_user
+    @tweeps = TWITTER_CLIENT.friends(current_user.username).to_a.sort_by { |user| user.followers_count }.reverse
   end
 end
