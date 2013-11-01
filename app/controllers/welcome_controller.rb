3452b974be5aee7151d44c7487eefb51c30ffe28
jleo3 <joseph.leo3@gmail.com>
2013-10-31 22:53:54 -0400

    back to the way things were

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @tweeps = TWITTER_CLIENT.friends(current_user.username).sort_by { |friend| friend.followers_count }.reverse
   end
 end
--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -2,5 +2,5 @@ TWITTER_CLIENT = Twitter::REST::Client.new do |config|
   config.consumer_key = "F1EbO7iQzBCtumPIWGxRLQ"
   config.consumer_secret = "9kGmSk6rIEffNauBE9rYEpHvzSBzIgd34AbFRvKtuo"
 
-  config.connection_options[:request] = {:open_timeout=>10, :timeout=>20}
+  config.connection_options[:request] = {:open_timeout=>20, :timeout=>40}
 end
