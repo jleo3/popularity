57bdf7ad66ec73e1295242001aa948a72e347934
jleo3 <joseph.leo3@gmail.com>
2013-11-04 14:32:20 -0500

    erroneous extra migrations

--- db/migrate/20131030190409_add_twitter_auth_to_user.rb
+++
@@ -1,8 +0,0 @@
-class AddTwitterAuthToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
-    add_column :users, :token, :string
-    add_column :users, :token_secret, :string
-  end
-end
